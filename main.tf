f2f21a960540044da565bf7c36bae910b94093fa
Taiwo <Bellotaiwoa30@gmail.com>
2023-08-15 14:50:04 +0100

    5th commit

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "google_container_cluster" "primary" {
 # Managed node pool
 
 resource "google_container_node_pool" "primary_nodes" {
-  name = "${google_container.primary.name}-node-pool"
+  name = "${google_container.primary}-node-pool"
   location = var.region
   cluster = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
@@ -25,7 +25,7 @@ resource "google_container_node_pool" "primary_nodes" {
         "https://www.googleapis.com/auth/monitoring",
     ]
     labels = {
-      env = vars.project_id
+      env = var.project_id
     }
 
     machine_type = "n1-standard-1"
